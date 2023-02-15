8d66d7b6440ef9c3d9d2e228485dc4f28bd842f9
yujuann <xxxnewzu@gmail.com>
2023-02-15 16:24:15 +0900

    setting hero, nav ,sectionsubtitle

--- components/UI/Hero.jsx
+++ components/UI/Hero.jsx
@@ -3,10 +3,23 @@ import SectionSubtitle from "./sectionSubtitle";
 import { Container, Row, Col } from "reactstrap";
 import Image from "next/image";
 import Link from "next/link";
-import HeroImg from "../../public/images/hero.jpg";
+// import HeroImg from "../../public/images/hero.jpg";
 import classes from "../../styles/hero.module.css";
 const Hero = () => {
-  return <div>Hero</div>;
+  return;
+  <section className={`${classes.hero}`}>
+    <Container>
+      <Row>
+        <Col lg="6" md="6">
+          <div className="{`${classes.hero__content}}">
+            <SectionSubtitle subtitle="Hello" />
+          </div>
+        </Col>
+        <Col lg="6" md="6"></Col>
+      </Row>
+    </Container>
+    <div>Hero</div>;
+  </section>;
 };
 
 export default Hero;
